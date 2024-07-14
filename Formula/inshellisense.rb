c4af04d96b516aac16db46e39228ef64343402ad
yarnaid <yarnaid@gmail.com>
2024-07-15 00:25:11 +0400

    build

--- Formula/inshellisense.rb
+++ Formula/inshellisense.rb
@@ -1,21 +1,19 @@
 class Inshellisense < Formula
-  desc "IDE style command line auto complete"
+  desc "IDE style autocomplete for shells"
   homepage "https://github.com/microsoft/inshellisense"
-  url "https://github.com/microsoft/inshellisense/archive/refs/tags/v0.3.24.tar.gz"
-  sha256 "48f744155a6d88ff31aadf94c38f80ab6bba131c75a2fc366ce11fc3e6c5bbc8"
+  url "https://registry.npmjs.org/@microsoft/inshellisense/-/inshellisense-0.0.1-rc.15.tgz"
+  sha256 "e171ef357191c114422789e4f7a5aab37eba7e01a71f3fcd36d37eff8e1e835b"
   license "MIT"
 
-  depends_on "rust" => :build
-  depends_on "node" => :build
-  depends_on "yarn" => :build
+  depends_on "node"
 
   def install
-    system "yarn", "install"
-    system "yarn", "build"
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "npm", "install", "-g", "inshellisense-0.0.1-rc.15.tgz", "--prefix=#{libexec}"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    assert_match "InShellisense", shell_output("#{bin}/inshellisense --version")
+    system "#{bin}/is", "--version"
   end
 end
+
